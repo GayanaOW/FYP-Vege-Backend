b3ab767cf773328133f21cb6cc395088837f6f7d
GayanaOW <gayana.waraketiya@gmail.com>
2026-03-20 00:17:38 +0530

    C3

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT,
+  port: Number(process.env.MYSQLPORT),
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
