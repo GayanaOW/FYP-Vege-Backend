7aa6e4ac17532b0a8633b2d791452e35c092fde3
GayanaOW <gayana.waraketiya@gmail.com>
2026-03-20 18:59:57 +0530

    sql create pool update

--- src/db.js
+++ src/db.js
@@ -1,16 +1,18 @@
 const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  port: Number(process.env.MYSQLPORT),
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+const pool = mysql.createPool(process.env.MYSQL_PUBLIC_URL);
 
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+// const pool = mysql.createPool({
+//   host: process.env.MYSQLHOST,
+//   port: Number(process.env.MYSQLPORT),
+//   user: process.env.MYSQLUSER,
+//   password: process.env.MYSQLPASSWORD,
+//   database: process.env.MYSQLDATABASE,
+
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
 
 // Test connection once at startup
 (async () => {
